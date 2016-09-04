aa68780de5c0115b584b97ab998bcad49d691b10
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-09-04 11:37:37 -0700

    simple hide/show text

--- app/src/main/java/com/example/clarkhinchcliff/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/simpleapp/MainActivity.java
@@ -2,8 +2,29 @@ package com.example.clarkhinchcliff.simpleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    boolean textHidden = false;
+
+    public void hideText(View view){
+        if(textHidden == false){
+            textView = (TextView)findViewById(R.id.textView);
+            textView.setVisibility(View.GONE);
+            textHidden = true;
+        }
+    }
+
+    public void showText(View viw){
+        if(textHidden == true) {
+            textView = (TextView)findViewById(R.id.textView);
+            textView.setVisibility(View.VISIBLE);
+            textHidden = false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
